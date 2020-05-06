bcf98caef8367d0010dc376c7636b77d15f44b24
Rhonda Ward Crespo <rcrespo@montedelsol.org>
2020-05-06 09:28:43 -0600

    I feel like the plot is so close...

--- epimodel.py
+++ epimodel.py
@@ -29,36 +29,42 @@ def main():
     t_max = 100 #length of time
     dt = 0.1 #change in time
     t = np.linspace(0, t_max, int(t_max/dt) + 1) #sets up graph
-    init_values = 1 - 1/N, 1/N, 0
+    init_values = 1 - 1/N, 1/N, 0 #sets up initial values for S, I, R
     beta = 2.0 #contact rate in the population
     gamma = 1.0 #inverse of the mean infectious period
     results = sir_model(N, t, dt, beta, gamma, init_values) #runs the model
-
+    #y = S, I, R
+    
 #these are the differential equations that calculate the SIR model
 def sir_model(N, t, dt, beta, gamma, init_values):
-    S_0, I_0, R_0 = init_values
-    S, I, R = [S_0], [I_0], [R_0]
-    dt = t[1] - t[0]
-    for i in t:
+    S_0, I_0, R_0 = init_values #initial values for SIR
+    S, I, R = [S_0], [I_0], [R_0] #empty lists for SIR
+    dt = t[1] - t[0] #change in time
+    for i in t: #these are the actual calculations 
         next_S = int(S[-1] - (beta*S[-1]*I[-1])*dt)
         next_I = int(I[-1] + (beta*S[-1]*I[-1]/N-gamma* I[-1]*dt))
         next_R = int(R[-1] + (gamma*I[-1]*dt))
-        S.append(next_S)
+        S.append(next_S) #these add the calculations to the bottom of each list
         I.append(next_I)
         R.append(next_R)
-    return np.stack([S, I, R]).T
-print('this actually worked')
+    return np.stack([S, I, R]).T #returns the values
+print('this actually worked') #I just needed to see that it worked!
 
 #plot the data as three separate lines for S, I, and R
-fig = plt.figure()
-ax = plt.axes()
-x = np.linspace(0, 10, 1000)
-ax.plot(x, S, color='blue', label='Susceptible')
-ax.plot(x, I, color='red', label='Infectious')
-ax.plot(x, R, color='green', label='Removed')
-ax.set_xlabel('Time(Days)')
-ax.set_ylabel('Number of Individuals')
-plot.show()
+def plot_fun(t, S, I, R):
+    fig = plt.figure()
+    ax = plt.axes()
+    x = np.linspace(0, 10, 1000)
+    ax.plot(t, S, color='blue', label='Susceptible')
+    ax.plot(t, I, color='red', label='Infectious')
+    ax.plot(t, R, color='green', label='Removed')
+    ax.set_xlabel('Time(Days)')
+    ax.set_ylabel('Number of Individuals')
+    plot.show()
+    #fig, ax = plt.subplots(1)
+    #plot_fun(t, S, I, R)
+    fig.savefig('epidemic.png')
+    return (fig)
 
 
 #call the main function
